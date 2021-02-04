9e7ef735dd6e3b29184e5b96b8461a2727443883
AtiaK <atiakalwar@gmail.com>
2021-02-04 14:29:31 +0500

    Auth

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 
                           //your name:your password @clustername    
-dbPassword = 'mongodb://localhost:27017/test?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+dbPassword = //DB Password
 
 module.exports = {
     mongoURI: dbPassword
